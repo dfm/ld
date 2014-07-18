4305072ec99f656acb3bd2c986e394ae6e655b02
Dan F-M <danfm@nyu.edu>
2014-07-18 17:21:00 +0000

    the Ks need to transform too

--- cases/case-10.m
+++ cases/case-10.m
@@ -1,10 +1,11 @@
 (* Define the geometric model *)
-k1 := ArcCos[(1 - p^2 + z^2) / (2*z)];
-k0 := ArcCos[(p^2 + z^2 - 1) / (2*p*z)];
+k1 := Simplify[ArcCos[(1 - (p/r)^2 + (z/r)^2) / (2*(z/r))]];
+k0 := Simplify[ArcCos[((p/r)^2 + (z/r)^2 - 1) / (2*p*z/r^2)]];
 Fe := 1 - Piecewise[{
     {0, 1+p/r<z/r},
     {((p/r)^2 * k0 + k1 - Sqrt[(z/r)^2 - ((1+(z/r)^2-(p/r)^2)^2)/4])/Pi,
-           Abs[1-(p/r)] < (z/r) && (z/r) <= 1+(p/r)},
+        (((p/r <= 1) && 1-(p/r) < (z/r)) || ((p/r > 1) && (p/r)-1 < (z/r)))
+        && (z/r) <= 1+(p/r)},
     {(p/r)^2, (z/r) <= 1-(p/r)},
     {1, (z/r) <= (p/r) - 1}
 }];
